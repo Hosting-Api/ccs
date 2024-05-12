2941ac89d5ac8140f14abe1edd07a6bff8c50cde
Hosting-Api <131949339+Hosting-Api@users.noreply.github.com>
2024-05-12 10:52:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        formats: ["image/avif", "image/webp"],
+        domains: ["s.gravatar.com"]
+    },
     reactStrictMode: true,
         
     
