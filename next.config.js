d6faf5e0d1eec14663bea2d9fca6714c63e3403a
Hosting-Api <131949339+Hosting-Api@users.noreply.github.com>
2024-05-12 11:30:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-       unoptimized: true,
-},
-
+    basePath: '/ccs',
+images: {
+        formats: ["image/avif", "image/webp"],
+    },
     reactStrictMode: false,
         
     
